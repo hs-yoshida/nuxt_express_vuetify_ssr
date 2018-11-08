ee56c57c52b5d06047877f21e9c69652222e857b
hs-yoshida <hirofumi-yoshida@science.co.jp>
2018-11-08 10:03:21 +0900

    2018/11/08_10:03:21.46

---
+++ pages/head_sample.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ paragraph }}</p>
+    <img src="~/assets/image.png">
+    <v-img :src="require('~/assets/image.png')" />
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Hello World!',
+      paragraph: 'Hi there!'
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: 'My custom description' }
+      ]
+    }
+  },
+  body () {
+    return {
+      paragraph: this.paragraph,
+    }
+  }
+}
+</script>
